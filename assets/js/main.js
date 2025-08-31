64771a7dbd470c879c184a40e5acd9806f588c4c
Thiago Lelles Fernandes <thilelles@gmail.com>
2025-08-31 16:13:17 -0300

    Adicionado código para experiencia profissional

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,20 @@ function updatePortifolio(profileData) {
 	}).join("");
 }
 
+function updateProfessionalExperience(profileData) {
+	const professionalExperience = document.getElementById("profile.professionalExperience");
+
+	professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+		return `
+		<li>
+			<h3 class="title">${experience.name}</h3>
+			<p class="period">${experience.period}</p>
+			<p>${experience.description}</p>
+		</li>
+		`
+	}).join("");
+}
+
 
 (async () => {
 	const profileData = await fetchProfileData();
@@ -61,4 +75,5 @@ function updatePortifolio(profileData) {
 	updateHardSkills(profileData);
 	updateLanguages(profileData);
 	updatePortifolio(profileData);
+	updateProfessionalExperience(profileData);
 })()
