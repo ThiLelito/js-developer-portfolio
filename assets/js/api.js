97cde17817db1d18c18d2b62474e79856b47c3bc
Thiago Lelles Fernandes <thilelles@gmail.com>
2025-08-31 15:42:50 -0300

    Atualização dos dados

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,15 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
-}
-
-
-
-async function fetchProfileData() {
-	const url = 'https://raw.githubusercontent.com/ThiLelito/js-developer-portfolio/refs/heads/projeto-base/data/profile.json';
+	const url = 'https://raw.githubusercontent.com/ThiLelito/js-developer-portfolio/refs/heads/main/data/profile.json';
 	const fetching = await fetch(url);
 	return await fetching.json();
 }
